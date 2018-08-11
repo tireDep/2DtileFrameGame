23914733382f38ce4204d31af984eeea1e34dc6c
tireDep <wing997@naver.com>
2018-08-11 11:57:51 +0900

    창 종료시 파괴, 주석 정리, msg 부분 수정

--- 2DtileFrame/TileFrameGame/main.cpp
+++ 2DtileFrame/TileFrameGame/main.cpp
@@ -1,17 +1,22 @@
 #include <Windows.h>
-// 윈도우 프로그래밍은 main을 직접 사용하면 안 됨!
-// 내부적으로 정상적인지 판별한 후, window가 WinMain() 실행함
-// 차이점 : main은 에러나도 실행됨, Winmain은 윈도우가 실행 안 시켜주면 실행 안됨
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)	// 윈도우 프로시저 함수 : 윈도우로부터 받은 이벤트를 처리하는 함수(내가 처리함)
-{	// CALLBACK 있으면 번쩍하고 사라짐(이거는 찾아보기) 근데 없으면 실행이 안되네..?
-	// 언제 이벤트가 발생하고, 왜 일어나는지는 모름 -> 다른 곳에서 발생한 이벤트가 일어날 수도 있음(최악)
+{
 	// 이벤트를 받을 공간 하나만 생성함(아직까지는 윈도우가 다 처리해줌)
 	switch (msg)	// msg가 곧 이벤트
 	{
 	case WM_LBUTTONDOWN:	// 마우스 왼쪽 버튼이 눌림
 		MessageBox(0, "Hello World", "Hello", MB_OK);	// 0 : 일반적인 창에 영향을 받지 않음 => WindowOS에 직접 관리를 받음! / 창을 넣을 경우에는 창에 따라 바뀜
 		return 0;
+	case WM_KEYDOWN:	// 키 입력
+		if (VK_ESCAPE == wParam)	// 입력키가 esc
+		{
+			DestroyWindow(hWnd);	// 창 파괴 msg
+			// !주의! - PostQuitMessage(0); 작성 시 파괴 msg가 발급 x이므로 실행되지 않음
+		}
+		break;
+	case WM_DESTROY:	// 파괴 msg
+		PostQuitMessage(0);
 		break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
@@ -20,12 +25,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)	// �
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 // 현재 응용 프로그램의 메인 핸들, 안쓰임, 실행하는데 쓰이는 명령줄 도는 인수, 응용프로그램 표시 방식(최소/최대화 해서 보여줄거냐 등)
 {
-	// 3.
-	// 윈도우 스타일을 만들고 등록함
+	// 3. 윈도우 스타일을 만들고 등록함
 	WNDCLASS wc;
 	wc.style = CS_HREDRAW | CS_VREDRAW;	// 창의 스타일 지정(화면 크기를 바꿀때마다 글씨가 보이고 안보이고를 갱신하는 것)
 	wc.lpfnWndProc = WndProc;	// 윈도우 프로시저 함수 등록(처리 관련) 
-	// 근데 이거 함수아닌가 왜 괄호없음? -> 함수포인터!(함수가 어디에 있는지만 알려줌)
+	// 함수포인터!(함수가 어디에 있는지만 알려줌)
 	// 형태가 약속되어 있기 때문에 작동이 가능함! 다른 형태일 경우 작동되지 않음!!(요거도 찾아봅시다)
 	wc.cbClsExtra = 0;	// 사용x(사장됨) / 안해줘도 되긴 하는데 명시적으로는 작성해줌
 	wc.cbWndExtra = 0;	// 사용x(사장됨) / 안해줘도 되긴 하는데 명시적으로는 작성해줌
@@ -49,21 +53,54 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 	if (NULL == hWnd)	// 정상적으로 실행이 안될 경우
 		return 0;
 
-	// 윈도우는 발급을 안해주면 띄울 수 없음!
-
 	// 1.
 	ShowWindow(hWnd, nCmdShow);	// hWnd : 하나의 윈도우를 가리키는 핸들(ID)
 	UpdateWindow(hWnd);
 	// hWnd란 이름을 가진 윈도우를 보여주고 업데이트 해줌
 
 	// 윈도우가 꺼지지 않고 유지되도록 처리
-	// 담주에 수정시작
-	MSG msg;
+	/*MSG msg;
 	while (-1 != GetMessage(&msg, 0, 0, 0))	// 발생된 메시지가 있으면
 	{
 		TranslateMessage(&msg);	// 키보드 변환 수행
 		DispatchMessage(&msg);	// 메시지 배분을 요청함
 	}
+	*/
+
+	// 게임의 경우 - msg가 있으면 있는대로, 없으면 없는 대로 처리되어야 함
+	// 게임에 맞게 개조된 부분
+	MSG msg = { 0 };
+	// 윈도우성능 중 최대로 실행
+	while (WM_QUIT != msg.message)	// app 종료 != destroy
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE) == TRUE)	// window는 window msg 무시x -> 최우선 순위!
+		{
+			/* 
+			! 차이점 !
+			GetMessage -> msg가 있을 경우에만 나옴 / 언제 나올지 모름 => 이벤트가 있을 경우에만 사용함
+			PeekMessage -> msg 유무에 상관 없이 나옴 / 한턴내에 처리됨 => 이벤트 상관 x로 사용함
+			*/
+			TranslateMessage(&msg);	// 키보드 변환 수행
+			DispatchMessage(&msg);	// 메시지 배분을 요청함
+		}
+		else
+		{
+			// 이론상으로는 들어갈 수 없는데 그게 1초에 만번 연타
+			// 정수로 할 수도 있음 -> 틱 사용
+			float frameTime = 0;
+			float frameInterval = 1.0f / 60.0f;	// frameInterval로 frame 조정
+			if (frameInterval <= frameTime)	// 프레임 시간 <= 흐른 시간 일 경우 Update
+			{
+				/*
+				! todo !
+				- gameUpdate .60fps
+				- 전체 중 60fps로 제한하는 것 => 게임 while문과 상관 x
+				*/
+				frameTime = 0.0f;
+			}
+		}
+	}
+
 	return 0;
 
 	/* 순서
