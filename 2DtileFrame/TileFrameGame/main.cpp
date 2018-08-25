3f0c7b8f45d3592f8fd2810767d10ae61f49062f
tireDep <wing997@naver.com>
2018-08-25 10:06:36 +0900

    window -> fullscreen으로 변경함 1) window style 변경 - window와 연관있음, directX라는 상관x 2) directX 색상 변경 - directX랑 직접 상관 있음

--- 2DtileFrame/TileFrameGame/main.cpp
+++ 2DtileFrame/TileFrameGame/main.cpp
@@ -1,3 +1,4 @@
+// Setting
 #include <Windows.h>
 #include <d3d9.h>
 #include "GameTimer.h"
@@ -24,9 +25,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 {
 	int width = 1024;
 	int height = 768;
-	bool isWindow = true;
-	// 전체화면 과제 해결
-	// bool isWindow = false;
+	bool isWindow = false;
 
 	WNDCLASS wc;
 	wc.style = CS_HREDRAW | CS_VREDRAW;	
@@ -43,9 +42,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 	if (RegisterClass(&wc) == FALSE)
 		return 0;
 
-	HWND hWnd = CreateWindow("2DTileFrameWnd", "2D Tile Frame", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width, height, 0, 0, hInstance, 0);
-	// 전체화면 과제 해결
-	// HWND hWnd = CreateWindow(TEXT("2DTileFrameWnd"), TEXT("2DTileFrameWnd"), WS_EX_TOPMOST | WS_POPUP, 0, 0, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hInstance, 0);
+	DWORD style;
+	if (isWindow)	// 윈도우 모드일 경우 처음 창 요소 그대로 사용
+		style = WS_OVERLAPPEDWINDOW;
+	else // 풀스크린 모드인 경우, 윈도우에 부가적인 요소를 제거함
+		style = WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP;	// 윈도우 구상 가장 기본적인 요소
+	// 윈도우화면 성능과 관련됨, 다이렉트엑스랑 상관x
+	
+	HWND hWnd = CreateWindow("2DTileFrameWnd", "2D Tile Frame", style, CW_USEDEFAULT, CW_USEDEFAULT, width, height, 0, 0, hInstance, 0);
 
 	if (NULL == hWnd)
 		return 0;
@@ -67,12 +71,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 	if (direct3D == NULL)
 		return 0;
 
+	D3DFORMAT format;
+	if (isWindow)	// 윈도우 설정에 따름
+		format = D3DFMT_UNKNOWN;
+	else // 풀스크린일때 전용 색상 포멧을 사용함
+		format = D3DFMT_X8R8G8B8;
+	// 다이렉트x랑 직접적인 연관 있음
+
 	D3DPRESENT_PARAMETERS d3dpp;	
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
 
 	d3dpp.BackBufferWidth = width;
 	d3dpp.BackBufferHeight = height;
-	d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
+	d3dpp.BackBufferFormat = format;
 	d3dpp.BackBufferCount = 1;
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
 	d3dpp.hDeviceWindow = hWnd;
@@ -81,10 +92,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 	
 	LPDIRECT3DDEVICE9 dxDevice;
 	HRESULT hr = direct3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &dxDevice);
+	// lost device 관련 설정 해줘야함
 
 	if (FAILED(hr))
 		return 0;
-
 
 	float fps = 60.0f;
 	float frameInterval = 1.0f / fps;
@@ -113,6 +124,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 				dxDevice->Present(NULL, NULL, NULL, NULL);
 			}
 		}
+	}
+
+	// 컴 인터페이스들 생성되있을 경우 삭제 작업 필요함
+	if (dxDevice)	
+	{
+		dxDevice->Release();
+		dxDevice = NULL;
+	}
+	if (direct3D)
+	{
+		direct3D->Release();
+		direct3D = NULL;
 	}
 	return 0;
 }
