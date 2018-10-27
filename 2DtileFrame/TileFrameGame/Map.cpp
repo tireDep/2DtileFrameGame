7e998137b457260ce9e3ea650fb7bf43a4c50a98
tireDep <wing997@naver.com>
2018-10-27 12:12:54 +0900

    타일맵 스크립트화

--- 2DtileFrame/TileFrameGame/Map.cpp
+++ 2DtileFrame/TileFrameGame/Map.cpp
@@ -1,4 +1,7 @@
-#include"Sprite.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <fstream>
+#include <string>
+#include "Sprite.h"
 #include "Map.h"	
 // 관련 인클루드는 맨 아래에 두는 것이 인클루드 관리에 편함
 
@@ -32,8 +35,9 @@ void Map::Init(LPDIRECT3DDEVICE9 dxDevice, ID3DXSprite* spriteDX)
 	// 맵용 스프라이트 리스트 작업
 	// 타일 개수, 개별 타일 크기는 합의가 끝난 상태(팀 작업일 경우) => 32x32, 256개
 
+	// 맵 생성
 	int tileMapIndex[16][16];	// 타일맵 변수
-	int idx = 0;	// 순서 체크 변수
+	/*int idx = 0;	// 순서 체크 변수
 
 	for (int y = 0; y < 16; y++)
 	{
@@ -43,8 +47,41 @@ void Map::Init(LPDIRECT3DDEVICE9 dxDevice, ID3DXSprite* spriteDX)
 			tileMapIndex[y][x] = idx++; //0 ~ 255
 		}
 	}	// 타일맵 정보 저장
+	*/
+	// 스크립트 대체
+	{
+		std::string fileName = "map.csv";	// 파일 이름
+		std::vector<std::string>recordList;	// 토큰 집합
+		
+		// file open -> file read(record) -> parshing(token) -> save
+		std::ifstream infile(fileName);	// 파일 읽어옴!!
+		if (true == infile.is_open())	// 파일이 정상적으로 열릴 경우
+		{
+			char recordString[1000];
+			while (false == infile.eof())	// 파일 마지막까지 루프
+			{
+				infile.getline(recordString, 1000);	// 한 줄씩 읽어옴
+				recordList.push_back(recordString);	// 레코드에 저장
+			}
+		}
 
-	LPCWSTR scriptName;	// 스크립트 생성
+		// 레코드 -> 토큰, 게임에 적용
+		char record[1000];
+		for(int y = 0; y < 16; y++)	// 16개 레코드가 16줄 존재
+		{
+			strcpy(record, recordList[y].c_str());	// 통째로 한 줄 복사
+			char *token = strtok(record, ",");	// 분리가 된 첫 번째 토큰이 record에 저장됨(문자로)
+
+			for (int x = 0; x < 16; x++)
+			{
+				int tileIndex = atoi(token);	// 문자 -> 숫자 저장
+				tileMapIndex[y][x] = tileIndex;
+				token = strtok(NULL, ",");	// 다음거 저장 후 parse
+			}
+		}
+
+	}
+	// LPCWSTR scriptName;	// 스크립트 생성
 
 	for (int y = 0; y < 16; y++)
 	{
